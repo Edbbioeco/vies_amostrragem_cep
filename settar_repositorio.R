f1f4b7f83438b4cadb0d99b4baa4222966568f09
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-12 12:18:02 -0300

    Script para settar o repositório do projeto

---
+++ settar_repositorio.R
@@ -0,0 +1,19 @@
+# Pacotes ----
+
+library(usethis)
+
+library(gert)
+
+# Iniciar git ----
+
+usethis::use_git()
+
+# Primeiro commit ----
+
+gert::git_add(files = ".gitignore")
+
+gert::git_commit(message = ".gitignore")
+
+# Criar repositório ----
+
+usethis::use_github()
