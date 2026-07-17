3198af06ee22454aab8fa0c9ccb942be5c2b79e3
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-17 18:59:11 -0300

    Importar e visulizar o shapefile do CEP

--- criar_shapefiles_preditores_vieses.R
+++ criar_shapefiles_preditores_vieses.R
@@ -7,3 +7,16 @@ library(geobr)
 library(tidyverse)
 
 library(osmdata)
+
+# Shapefile do CEP ----
+
+## Importar ----
+
+cep <- sf::st_read("cep.shp")
+
+## Visualizar ----
+
+cep
+
+ggplot() +
+  geom_sf(data = cep, color = "black")
