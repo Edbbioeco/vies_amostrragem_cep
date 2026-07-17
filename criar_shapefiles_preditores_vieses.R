1315d6ac81824f15e72c8e33f78ab9e20dbfb826
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-17 19:22:27 -0300

    Ikmportar e visualizar o shapefile das unidades de conservação

--- criar_shapefiles_preditores_vieses.R
+++ criar_shapefiles_preditores_vieses.R
@@ -20,3 +20,17 @@ cep
 
 ggplot() +
   geom_sf(data = cep, color = "black")
+
+# Unidades de conservação ----
+
+## Importar ----
+
+uc <- geobr::read_conservation_units(date = "202503")
+
+## Visualiza ----
+
+uc
+
+ggplot() +
+  geom_sf(data = uc, color = "darkgreen", fill = "forestgreen") +
+  geom_sf(data = cep, color = "red", fill = "transparent")
