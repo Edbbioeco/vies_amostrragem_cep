684da1de6d275eb20cb865f5a3bb5e945ffc5502
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-17 20:20:56 -0300

    Extrair a localização das universidades do Open Street Map

--- criar_shapefiles_preditores_vieses.R
+++ criar_shapefiles_preditores_vieses.R
@@ -49,3 +49,36 @@ ggplot() +
 ## Exportar o shapefile ----
 
 uc_rec |> sf::st_write("unidade_conservacao_cep.shp")
+
+## Localização das universidades ----
+
+## Importar ----
+
+estados_cep <- c("Pernambuco",
+                 "Alagoas",
+                 "Paraiba",
+                 "Rio Grande do Norte")
+
+universidades_estados <- purrr::map(estados_cep,
+                                    purrr::in_parallel(
+        ~osmextract::oe_get(
+          place = .x,
+          provider = "geofabrik",
+          layer = "points",
+          extra_tags = c("amenity", "name"),
+          query = "SELECT * FROM points WHERE amenity = 'university'",
+          force_download = FALSE,
+          quiet = TRUE)
+
+        ),
+        .progress = TRUE) |>
+  dplyr::bind_rows()
+
+## Visualizar ----
+
+universidades_estados
+
+ggplot() +
+  geom_sf(data = uc_rec, color = "darkgreen", fill = "forestgreen") +
+  geom_sf(data = cep, color = "red", fill = "transparent") +
+  geom_sf(data = universidades_estados, color = "black")
