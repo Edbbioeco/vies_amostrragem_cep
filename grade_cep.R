5a494b94b32f062348bae8afe7d7615757062913
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-18 20:16:50 -0300

    Importar e visualizar o shapefile do CEP

--- grade_cep.R
+++ grade_cep.R
@@ -3,3 +3,16 @@
 library(sf)
 
 library(tidyverse)
+
+# Ácrea do CEP ----
+
+## Importar ----
+
+cep <- sf::st_read("cep.shp")
+
+## Visualizar ----
+
+cep
+
+ggplot() +
+  geom_sf(data = cep, color = "black")
