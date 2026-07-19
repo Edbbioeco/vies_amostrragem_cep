fecb0802b6c343ad186ae7a10610cc6f0e569220
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-19 11:30:35 -0300

    Lista de pacotes do script

---
+++ calcular_vies_amostragem.R
@@ -0,0 +1,19 @@
+# Pacotes ----
+
+library(readxl)
+
+library(tidyverse)
+
+library(sf)
+
+library(sampbias)
+
+library(terra)
+
+library(writexl)
+
+library(tidyterra)
+
+library(ggtext)
+
+library(ggview)
