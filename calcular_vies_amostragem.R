470ec258a943f94db5a51887f5e772d363aa5a3d
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-19 11:43:46 -0300

    Importar e visualizar o shapefile das unidades de conservação

--- calcular_vies_amostragem.R
+++ calcular_vies_amostragem.R
@@ -50,3 +50,20 @@ ggplot() +
              aes(Longitude, Latitude),
              size = 1) +
   geom_sf(data = cep, color = "black", fill = "transparent")
+
+## Unidades de conservação ----
+
+### Importar ----
+
+uc <- sf::st_read("unidade_conservacao_cep.shp")
+
+### Visualizar ----
+
+uc
+
+ggplot() +
+  geom_point(data = registros,
+             aes(Longitude, Latitude),
+             size = 1) +
+  geom_sf(data = cep, color = "black", fill = "transparent") +
+  geom_sf(data = uc, color = "red", fill = "transparent")
