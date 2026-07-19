d26d46f29001d3dce71083f6a664809a28918c09
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-19 10:53:42 -0300

    Importar e visualizar os registros de ocorrência

--- raster_riqueza.R
+++ raster_riqueza.R
@@ -11,3 +11,20 @@ library(terra)
 library(tidyterra)
 
 library(ggview)
+
+# Dados ----
+
+## Registros ----
+
+### Importar ----
+
+registros <- readxl::read_xlsx("registros.xlsx")
+
+### Visualizar ----
+
+registros
+
+registros |> dplyr::glimpse()
+
+ggplot() +
+  geom_point(data = registros, aes(Longitude, Latitude))
